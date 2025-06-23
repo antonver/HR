7d6542d44721fde437fd44f89cf254c12d7e2fa5
antonver <anton.verbovyi@gmail.com>
2025-06-23 04:05:16 +0200

    feat(instance): Add Telegram user data to POST requests

--- src/utils/instance.js
+++ src/utils/instance.js
@@ -1,15 +1,28 @@
 import axios from 'axios';
-import { retrieveRawInitData } from '@telegram-apps/sdk'
+import { retrieveRawInitData, parseInitData } from '@telegram-apps/sdk';
 
-const initDataRaw = retrieveRawInitData()
-
+const initDataRaw = retrieveRawInitData();
 const baseInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
-        "Authorization": `tma ${initDataRaw}`
+        'Authorization': `tma ${initDataRaw}`,
     },
+});
+
+// Добавляем interceptor для POST-запросов
+baseInstance.interceptors.request.use((config) => {
+    if (config.method === 'post') {
+        const initData = parseInitData();
+        config.headers['x-telegram-user-id'] = initData.user?.id || null;
+        config.headers['x-telegram-username'] = initData.user?.username || null;
+        config.headers['x-telegram-first-name'] = initData.user?.first_name || null;
+        config.headers['x-telegram-last-name'] = initData.user?.last_name || null;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
 });
 
 export default baseInstance;
